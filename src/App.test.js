3d532cd181da5e12c021c69b27f89de6fa81a27b
RawJrk <rodjer7777@gmail.com>
2023-06-16 17:02:41 +0300

    simplified test suite

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,8 @@
-import { screen, render, cleanup } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import App from "./App";
 
-describe("App component", () => {
-  beforeAll(() => {
-    render(<App />);
-  });
-
-  it("should have the right message in the dom", () => {
-    const message = "Hi, stranger!";
-    expect(screen.getByText(message)).toBeInTheDocument();
-  });
-
-  afterAll(cleanup);
+test("should have the right message in the dom", () => {
+  render(<App />);
+  const message = "Hi, stranger!";
+  expect(screen.getByText(message)).toBeInTheDocument();
 });
